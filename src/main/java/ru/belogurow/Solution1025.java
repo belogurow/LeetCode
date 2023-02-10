78a3a9130fa23f3dd81a93b420a71753524efb37
belogurow <alexbelogur10@gmail.com>
2023-02-10 18:13:48 +0300

    add Solution1025

---
+++ src/main/java/ru/belogurow/Solution1025.java
@@ -0,0 +1,11 @@
+package ru.belogurow;
+
+/**
+ * @see <a href="https://leetcode.com/problems/divisor-game/">1025. Divisor Game</a>
+ */
+public class Solution1025 {
+
+	public boolean divisorGame(int n) {
+		return n % 2 == 0;
+	}
+}
