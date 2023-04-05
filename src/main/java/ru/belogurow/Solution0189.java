5ea0040c8ed3624ad4531a14f57942a92643c0a4
belogurow <alexbelogur10@gmail.com>
2023-04-05 13:11:46 +0300

    add Solution0189

---
+++ src/main/java/ru/belogurow/Solution0189.java
@@ -0,0 +1,25 @@
+package ru.belogurow;
+
+/**
+ * @see <a href="https://leetcode.com/problems/rotate-array/">189. Rotate Array</a>
+ */
+public class Solution0189 {
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] arr, int left, int right) {
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
