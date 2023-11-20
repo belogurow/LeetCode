bc7c0241c9f5c2bde131a6f9b2da6ef8f58e56f6
belogurow <alexbelogur10@gmail.com>
2023-11-20 19:38:28 +0300

    Add Solution2391

---
+++ src/main/java/ru/belogurow/Solution2391.java
@@ -0,0 +1,34 @@
+package ru.belogurow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @see <a href="https://leetcode.com/problems/removing-stars-from-a-string">LeetCode link</a>
+ */
+public class Solution2391 {
+
+    public int garbageCollection(String[] garbage, int[] travel) {
+        for (int i = 1; i < travel.length; i++) {
+            travel[i] += travel[i - 1];
+        }
+
+        int total = 0;
+        Map<Character, Integer> lastIdx = new HashMap<>();
+
+        for (int i = 0; i < garbage.length; i++) {
+            for (char c : garbage[i].toCharArray()) {
+                total += 1;
+                lastIdx.put(c, i);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : lastIdx.entrySet()) {
+            if (entry.getValue() > 0) {
+                total += travel[entry.getValue() - 1];
+            }
+        }
+
+        return total;
+    }
+}
