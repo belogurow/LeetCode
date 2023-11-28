63a6f74246878b9700c8a950cee46202997509d3
belogurow <alexbelogur10@gmail.com>
2023-11-28 19:14:30 +0300

    Add Solution2545

--- src/main/java/ru/belogurow/Solution2545.java
+++ src/main/java/ru/belogurow/Solution2545.java
@@ -1,8 +1,8 @@
 package ru.belogurow;
 
-import ru.belogurow.common.ListNode;
-
-import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * @see <a href="https://leetcode.com/problems/sort-the-students-by-their-kth-score/">2545. Sort the Students by Their Kth Score</a>
