e2ce058ffddad3d11f7a148319f41df8d4c7fa8a
belogurow <alexbelogur10@gmail.com>
2024-01-13 16:25:12 +0300

    Add Solution1347

--- src/main/java/ru/belogurow/Solution1347.java
+++ src/main/java/ru/belogurow/Solution1347.java
@@ -5,22 +5,22 @@ package ru.belogurow;
  */
 public class Solution1347 {
 
-	public int minSteps(String s, String t) {
-		int[] letters = new int[26]; // letters count
+    public int minSteps(String s, String t) {
+        int[] letters = new int[26]; // letters count
 
-		for (int i = 0; i < s.length(); i++) {
-			letters[s.charAt(i) - 'a'] += 1;
-			letters[t.charAt(i) - 'a'] -= 1;
-		}
+        for (int i = 0; i < s.length(); i++) {
+            letters[s.charAt(i) - 'a'] += 1;
+            letters[t.charAt(i) - 'a'] -= 1;
+        }
 
-		int moves = 0;
+        int moves = 0;
 
-		for (int letter : letters) {
-			if (letter > 0) {
-				moves += letter;
-			}
-		}
+        for (int letter : letters) {
+            if (letter > 0) {
+                moves += letter;
+            }
+        }
 
-		return moves;
-	}
+        return moves;
+    }
 }
