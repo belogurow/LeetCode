8b6f6e7d998d921fda5de2bb3a9b8429216b3203
belogurow <alexbelogur10@gmail.com>
2024-12-19 12:05:25 +0300

    Add Solution1475

---
+++ src/main/java/ru/belogurow/Solution1475.java
@@ -0,0 +1,21 @@
+package ru.belogurow;
+
+/**
+ * Implement strStr().
+ * @see <a href="https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/">1475. Final Prices With a Special Discount in a Shop</a>
+ */
+public class Solution1475 {
+
+	public int[] finalPrices(int[] prices) {
+		for (int i = 0; i < prices.length; i++) {
+			for (int j = i + 1; j < prices.length; j++) {
+				if (prices[j] <= prices[i]) {
+					prices[i] -= prices[j];
+					break;
+				}
+			}
+		}
+
+		return prices;
+	}
+}
